fa9452f68d83e360a8c6fe37093d410e879c93df
Jefferson Marques <jefferson.jsm3444@gmail.com>
2024-01-25 15:12:30 -0300

    Fundamentos de DDD - Dependências externas

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,11 +1,20 @@
 import { randomUUID } from "node:crypto"
 
+interface AnswerProps {
+  content: string
+  authorId: string
+  questionId: string
+}
+
 export class Answer {
   public id: string
   public content: string
-
-  constructor(content: string, id?: string) {
-    this.content = content
+  public authorId: string
+  public questionId: string
+  constructor(props: AnswerProps, id?: string) {
+    this.content = props.content
     this.id = id ?? randomUUID()
+    this.authorId = props.authorId
+    this.questionId = props.questionId
   }
 }
