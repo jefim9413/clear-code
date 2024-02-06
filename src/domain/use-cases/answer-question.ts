8ad59321b89d43c24268d459cfc3ed545238c128
Jefferson Marques <jefferson.jsm3444@gmail.com>
2024-02-06 18:58:01 -0300

    Fundamentos de DDD - Abstraindo criação de entidades

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { Answer } from "../entities/answer"
 import { AnswersRepository } from "../repositories/answers-repository"
 
@@ -12,10 +13,10 @@ export class AnswerQuestionUseCase {
     private answersRepository: AnswersRepository
   ) {}
   async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
-      authorId: instructorId,
+    const answer = Answer.create({
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
       content,
-      questionId
     })
 
     await this.answersRepository.create(answer)
