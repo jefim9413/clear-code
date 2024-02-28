3f5c20884737843fb7cb293dea19d8b7ab4dd843
Jefferson Marques <jefferson.jsm3444@gmail.com>
2024-02-28 19:26:40 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Editar resposta

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -3,5 +3,6 @@ import { Answer } from '../../enterprise/entities/answer'
 export interface AnswersRepository {
   findById(id: string): Promise<Answer | null>
   delete(answer: Answer): Promise<void>
+  save(answer: Answer): Promise<void>
   create(answer: Answer): Promise<void>
 }
